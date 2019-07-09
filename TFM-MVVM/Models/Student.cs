c381da2eed5b4213d4c19d2a6fd4033ce4eec2e6
Borja Guzman López <bguzman@plainconcepts.com>
2019-07-09 08:52:25 +0200

    otra prueba

--- TFM-MVVM/Models/Student.cs
+++ TFM-MVVM/Models/Student.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 
 namespace TFM_MVVM.Models
 {
-    
+
     public class Student
     {
         public Student()
         {
             Subjects = new List<Subject>();
         }
-    
+
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Surname { get; set; }    
+        public string Surname { get; set; }
         public virtual IEnumerable<Subject> Subjects { get; set; }
-        public int x {get;set;}
+        public int x { get; set; }
+        public int x1 { get; set; }
     }
 }
